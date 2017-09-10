96b34f97e4a9004b74534a911b6270239e6054ad
RareScrap <RareScrap@users.noreply.github.com>
2017-09-10 10:44:58 +0800

    Набросок работы с СМС сервером и новые иконки

--- app/src/main/java/com/apptrust/wingtaxi/LoginActivity.java
+++ app/src/main/java/com/apptrust/wingtaxi/LoginActivity.java
@@ -4,7 +4,11 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.view.PagerAdapter;
@@ -18,11 +22,27 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.apptrust.wingtaxi.fragments.SummaryFragment;
 import com.apptrust.wingtaxi.utils.NonSwipeableViewPager;
+import com.apptrust.wingtaxi.utils.ToastHandler;
 import com.github.vacxe.phonemask.*;
 
+import org.intellij.lang.annotations.Flow;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import static android.content.Intent.FLAG_ACTIVITY_REORDER_TO_FRONT;
 
@@ -54,6 +74,9 @@ public class LoginActivity extends AppCompatActivity {
     /** Если true, то UI на основной активити готов к работе и можно его показать */
     public static boolean mapReady = false;
     public static boolean dataReady = false;
+    private static boolean smsConfirmed = false;
+    
+    private String token;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -144,6 +167,22 @@ public class LoginActivity extends AppCompatActivity {
             // TODO: Добавить проверку валидности номера перед переходом к следующему полю
 
             //TODO Отправить введенный юзером код запрсом через ретрофит
+            if (viewPager.getCurrentItem() == 0) {
+                // Сохряняем номер телефона
+                SharedPreferences sharedPref = getSharedPreferences("phone", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("phone", phoneField.getText().toString());
+                editor.apply();
+
+                // Запрос на отправку данных
+                try {
+                    RegisterTask registerTask = new RegisterTask();
+                    registerTask.execute();
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
 
             // Тестовый сценарий
             if (viewPager.getCurrentItem() == 1) {
@@ -151,16 +190,17 @@ public class LoginActivity extends AppCompatActivity {
                 InputMethodManager inputMethodManager = (InputMethodManager) LoginActivity.this.getSystemService(Activity.INPUT_METHOD_SERVICE);
                 inputMethodManager.hideSoftInputFromWindow(LoginActivity.this.getCurrentFocus().getWindowToken(), 0);
 
-                // Проверяем введенный код
-                if (codeField.getText().toString().equals("666777")) {
-                    // Сохраняем номер телефона в главной активити
-                    // TODO: Сохранять в SharedPreferences
-                    MainActivity.phoneNumber = "test";
+                if (token != null) {
+                    ConfirmTask confirmTask = new ConfirmTask();
+                    confirmTask.execute(token, codeField.getText().toString());
+                } else {
+                    Toast.makeText(LoginActivity.this, "Вам еще не пришел код", Toast.LENGTH_SHORT).show();
+                }
 
-                    SharedPreferences sharedPref = getSharedPreferences("phone", Context.MODE_PRIVATE);
-                    SharedPreferences.Editor editor = sharedPref.edit();
-                    editor.putString("phone", phoneField.getText().toString());
-                    editor.apply();
+                // Проверяем введенный код
+                /*if (codeField.getText().toString().equals("666777")) {
+                    // Сохраняем номер телефона в главной активити
+                    MainActivity.phoneNumber = "test";
 
                     // Проверяем загрузилась ли карта во фрагменте на основной активити
                     if (!mapReady) {
@@ -187,7 +227,7 @@ public class LoginActivity extends AppCompatActivity {
                         Toast.makeText(LoginActivity.this, R.string.incorrect_auth_code, Toast.LENGTH_LONG).show();
                         return;
                     }
-                }
+                }*/
             }
 
             // Перелистываемся на следующее поле
@@ -245,7 +285,7 @@ public class LoginActivity extends AppCompatActivity {
         final TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
-                if (mapReady && dataReady) {
+                if (mapReady && dataReady && smsConfirmed) {
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     intent.setFlags(intent.getFlags() | FLAG_ACTIVITY_REORDER_TO_FRONT);
                     startActivity(intent);
@@ -257,5 +297,209 @@ public class LoginActivity extends AppCompatActivity {
             // TODO: Обработать ситуацию, когда карта так и не была готова
         };
         timer.schedule(timerTask, delay, period);
+    }
+
+    /** Хандлер для вывода toast-сообщений, инициируемых в других потоках */
+    private ToastHandler toastHandler = new ToastHandler(this);
+    
+    private class RegisterTask extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... params) {
+            // Достаем телефон из памяти устройства
+            SharedPreferences sharedPref = getSharedPreferences("phone", Context.MODE_PRIVATE);
+            String phone = sharedPref.getString("phone", "78005553535");
+
+            // Оставляем только цифры
+            phone = phone.replaceAll("[^0-9]", "");
+
+            // Формируем исходящий JSON
+            JSONObject json = new JSONObject();
+            try {
+                json.put("number", phone);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            // Отправляем POST на сервер и читаем ответ
+            try {
+                // Адрес, куда мы будем отсылать запрос для регистрации
+                URL url = new URL("http://romhacking.pw:8081/register");
+
+                // Формируем соединение
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(15000);
+                conn.setConnectTimeout(150000);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                conn.setRequestProperty("Secret", "c8df37bef1275f33");
+
+                // Формируем поток записи
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+
+                // Отправляем исходящий JSON
+                writer.write(json.toString());
+                writer.flush();
+                writer.close();
+                os.close();
+
+                // Пытаем прочитать ответ
+                if (conn.getResponseCode() == HttpsURLConnection.HTTP_OK) {
+                    // Извещаем ToastHandler об успешной отправке смс запроса
+                    toastHandler.obtainMessage(ToastHandler.SMS_REQUEST_SENT).sendToTarget();
+
+                    // Успешно читаем ответ
+                    String response = "";
+                    String line;
+                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    while ((line = br.readLine()) != null) {
+                        response += line;
+                    }
+                    return response;
+                } else {
+                    // Извещаем ToastHandler о неудачной отправке смс запроса
+                    toastHandler.obtainMessage(ToastHandler.CONNECTION_ERROR).sendToTarget();
+                }
+
+            } catch (Exception e) {
+                // Извещаем ToastHandler о неудачной отправке смс запроса
+                toastHandler.obtainMessage(ToastHandler.CONNECTION_ERROR).sendToTarget();
+
+                // Обрабатыаваем исключение
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        /**
+         * Обратотчик ответа сервера
+         * @param response Ответ сервера
+         */
+        @Override
+        protected void onPostExecute(String response) {
+            // TODO: Проверку на null
+
+            JSONObject responseJSON = null;
+            try {
+                responseJSON = new JSONObject(response);
+            } catch (JSONException e) {
+                // Извещаем ToastHandler о неудаче
+                toastHandler.obtainMessage(ToastHandler.SERVER_INTERNAL_ERROR).sendToTarget();
+                e.printStackTrace();
+            }
+
+            // TODO: Обрабатывать статус ответа
+
+            // Сохраняем токен
+            try {
+                token = responseJSON.getString("token");
+            } catch (JSONException e) {
+                // Извещаем ToastHandler об ошибке сервера
+                toastHandler.obtainMessage(ToastHandler.SERVER_INTERNAL_ERROR).sendToTarget();
+
+                // Обрабатыаваем исключение
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class ConfirmTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            // Формируем исходящий JSON
+            JSONObject json = new JSONObject();
+            try {
+                json.put("token", params[0]);
+                json.put("code", params[1]);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            // Отправляем POST на сервер и читаем ответ
+            try {
+                // Адрес, куда мы будем отсылать запрос для регистрации
+                URL url = new URL("http://romhacking.pw:8081/confirm");
+
+                // Формируем соединение
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(15000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                conn.setRequestProperty("Secret", "c8df37bef1275f33");
+
+                // Формируем поток записи
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+
+                // Отправляем исходящий JSON
+                writer.write(json.toString());
+                writer.flush();
+                writer.close();
+                os.close();
+
+                // Читаем ответ
+                if (conn.getResponseCode() == HttpsURLConnection.HTTP_OK) {
+                    String response = "";
+                    String line;
+                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    while ((line = br.readLine()) != null) {
+                        response+=line;
+                    }
+                    return response;
+                }
+                else {
+                    // Извещаем ToastHandler о неудачной отправке смс запроса
+                    toastHandler.obtainMessage(ToastHandler.CONNECTION_ERROR).sendToTarget();
+                }
+            } catch (Exception e) {
+                // Извещаем ToastHandler о неуспешной отправке запроса с кодом подтверждения
+                toastHandler.obtainMessage(ToastHandler.CONNECTION_ERROR).sendToTarget();
+
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        /**
+         * Обратотчик ответа сервера
+         * @param response Ответ сервера
+         */
+        @Override
+        protected void onPostExecute(String response) {
+            // TODO: Проверку на null
+
+            JSONObject responseJSON = null;
+            try {
+                responseJSON = new JSONObject(response);
+            } catch (JSONException e) {
+                // Извещаем ToastHandler о неуспешной отправке запроса с кодом подтверждения
+                toastHandler.obtainMessage(ToastHandler.SERVER_INTERNAL_ERROR).sendToTarget();
+                e.printStackTrace();
+            }
+
+            // TODO: Проверять статус ответа
+
+            // Сохраняем токен
+            try {
+                token = responseJSON.getString("password");
+                SharedPreferences sharedPref = getSharedPreferences("password", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("password", token);
+                editor.apply();
+
+                smsConfirmed = true;
+            } catch (JSONException e) {
+                // Извещаем ToastHandler об ошибке сервера
+                toastHandler.obtainMessage(ToastHandler.SERVER_INTERNAL_ERROR).sendToTarget();
+
+                // Обрабатыаваем исключение
+                e.printStackTrace();
+            }
+        }
     }
 }
